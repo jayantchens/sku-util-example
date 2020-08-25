3e6db290a96b7f02741bce25c9b4e11e104aaf9c
Jayant Chen <jayant@shoplineapp.com>
2020-08-25 10:43:17 +0800

    create example app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useMemo } from 'react';
+import SkuUtil from 'sku-util';
+import './app.css';
+import { data } from './data';
 
-function App() {
+function Sku () {
+  const [ specList ] = useState(data.skuSpec);
+  const [ specListData, setSpecListData] = useState([]); // 选中的规格属性数据对象
+  useMemo(() =>  SkuUtil.initSku(data.skuList), []);
+
+  function handleSpecAttr(item, index) {
+    const list = SkuUtil.getActionSpecList(specListData, item, index);
+    list && setSpecListData(list)
+  }
+
+  function checkOut() {
+    const validSpecLen = specListData.filter(item => item).length;
+    const specListLen = specList.length;
+    if (validSpecLen < specListLen) {
+      alert('请您检查是否有规格未选择')
+    } else {
+      alert('提交成功，请查看控制台数据')
+      console.log(specListData)
+    }
+  }
+
+  function transPrice() {
+    const price = SkuUtil.getPrice(specListData)
+    if (!price) return null;
+    if (price.maxPrice === price.minPrice) {
+      return `${price.maxPrice}元`;
+    }
+    return `${price.minPrice} ~ ${price.maxPrice}元`
+  }
+
+  function transSpec() {
+    return specListData.filter(item => item).map(item => item.name).join(';')
+  }
+
+  const selectSpec = transSpec();
+  const price = transPrice();
+  const stock = SkuUtil.getStock(specListData);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="skuWrapper">
+      {specList.map((item,index) => (
+        <div className="specBox" key={item.id}>
+          <div className="specName">{item.specName}</div>
+          <ul>
+            {item.specAttrList.map(attrItem => {
+              const disabled = SkuUtil.checkSpecAttrDisabled(specListData, attrItem.id, index)
+              const active = SkuUtil.checkSpecAttrActive(specListData, attrItem.id)
+              return (
+                <li 
+                  key={attrItem.id} 
+                  onClick={() => !disabled && handleSpecAttr(attrItem, index)}
+                  className={
+                    [
+                      disabled ? 'disabled' : '',
+                      active ? 'active' : ''
+                    ].join('')
+                }>{attrItem.name}</li>
+              )
+            })}
+          </ul>
+        </div>
+      ))}
+      <div className="selectedSpec" style={{marginTop: '20px'}}>选择的规格: {selectSpec || '---'}</div>
+      <div className="price">价格: {price ? price : '---'}</div>
+      <div className="stock">库存: {stock || stock === 0 ? stock : '---'}</div>
+      <div className="checkOut" onClick={checkOut}>立即购买</div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default Sku;
---
+++ src/data.js
@@ -0,0 +1,72 @@
+export const data = {
+  "skuList": {
+    // 4.7寸;16G;红色
+    "101;201;302": {
+      "img": 'https://img.alicdn.com/bao/uploaded/i2/O1CN01qJ8zzO24dezMvLpJV_!!2-juitemmedia.png_220x220q90.jpg',
+      "price": 200,
+      "stock": 10
+    },
+    // 4.7寸;16G;黄色
+    "101;201;303": {
+      "img": 'https://img.alicdn.com/bao/uploaded/i2/O1CN01qJ8zzO24dezMvLpJV_!!2-juitemmedia.png_220x220q90.jpg',
+      "price": 150,
+      "stock": 6
+    },
+    // 5.5寸;16G;红色
+    "102;201;302": {
+      "img": 'https://img.alicdn.com/bao/uploaded/i2/O1CN01qJ8zzO24dezMvLpJV_!!2-juitemmedia.png_220x220q90.jpg',
+      "price": 101,
+      "stock": 10
+    },
+    // 5.5寸;16G;黑色
+    "102;201;301": {
+      "img": 'https://img.alicdn.com/bao/uploaded/i2/O1CN01qJ8zzO24dezMvLpJV_!!2-juitemmedia.png_220x220q90.jpg',
+      "price": 200,
+      "stock": 0
+    },
+    // 5.5寸;64G;黄色
+    "102;203;303": {
+      "img": 'https://img.alicdn.com/bao/uploaded/i2/O1CN01qJ8zzO24dezMvLpJV_!!2-juitemmedia.png_220x220q90.jpg',
+      "price": 110,
+      "stock": 6
+    },
+    // 5.5寸;32G;黑色
+    "102;202;301": {
+      "img": 'https://img.alicdn.com/bao/uploaded/i2/O1CN01qJ8zzO24dezMvLpJV_!!2-juitemmedia.png_220x220q90.jpg',
+      "price": 300,
+      "stock": 16
+    }
+  },
+  "skuSpec": [
+    {
+      "id": 262,
+      "goodsId": 13,
+      "specName": "尺寸",
+      "specAttrList": [
+        { id: 101, specId: 262, name: '4.7寸' },
+        { id: 102, specId: 262, name: '5.5寸' },
+        { id: 103, specId: 262, name: '6.0寸' },
+      ]
+    },
+    {
+      "id": 263,
+      "goodsId": 13,
+      "specName": "内存",
+      "specAttrList": [
+        { id: 201, specId: 263, name: '16G' },
+        { id: 202, specId: 263, name: '32G' },
+        { id: 203, specId: 263, name: '64G' },
+      ]
+    },
+    {
+      "id": 264,
+      "goodsId": 13,
+      "specName": "颜色",
+      "specAttrList": [
+        { id: 301, specId: 264, name: '黑色' },
+        { id: 302, specId: 264, name: '红色' },
+        { id: 303, specId: 264, name: '黄色' },
+      ]
+    }
+  ]
+}
